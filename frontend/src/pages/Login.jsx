697994fbd2d8b348fa320518d7c191026cc809e3
dalsoya <lookat77@naver.com>
2022-03-25 15:52:55 +0900

    mern 3 rejected

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import { useState } from "react"
-import { FaSign, FaSignInAlt } from "react-icons/fa"
+import { useState, useEffect } from "react"
+import { FaSignInAlt } from "react-icons/fa"
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
+import { login, reset } from "../features/auth/authSlice"
+import Spinner from "../components/Spinner"
 
 function Login() {
   
@@ -10,6 +15,25 @@ function Login() {
 
     const { email, password } = formData
 
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+      if(isError) {
+        toast.error(message)
+      }
+
+      if(isSuccess || user) {
+        navigate('/')
+      }
+
+      dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
+
     const onChange = (e) => {
       setFormdata((prevState) => ({
         ...prevState,
@@ -19,9 +43,20 @@ function Login() {
 
     const onSubmit = (e) => {
       e.preventDefault()
+
+      const userData = {
+        email,
+        password,
+      }
+
+      dispatch(login(userData))
     }
 
-    return <>
+    if(isLoading) {
+      return <Spinner />
+    }
+
+    return (<>
       <section className="heading">
         <h1>
           <FaSignInAlt /> Login
@@ -42,7 +77,7 @@ function Login() {
           </div>
         </form>
       </section>
-    </>
+    </>)
 }
 
 export default Login
